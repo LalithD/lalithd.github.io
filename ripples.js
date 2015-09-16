f5132da5a282fa46c0b0b8b4d8c5a4ecb0785356
Lalith <lalith@uw.edu>
2015-09-16 15:33:15 -0700

    More Features
    Added controls for several more features and a hidden easter egg.

--- ripples.js
+++ ripples.js
@@ -15,9 +15,23 @@
         var initRadius = 5;
         var createSpeed = 20;
         var colorSpeed = 25;
+        var disappearTime = 1000;
+        var sizeIncrease = 4;
         document.addEventListener("mousemove", circle, false);
         document.addEventListener("mousedown", function() { if(click) {circles.push(createCircle());} }, false);
         document.addEventListener("keydown", checkKey, false);
+        document.getElementById("easter_egg").onclick = function() {
+            var getColor = document.getElementById("easter_egg").style.color;
+            var colorArray = ["rgb(255, 0, 0)", "rgb(255, 165, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)", "rgb(75, 0, 130)", "rgb(238, 130, 238)"];
+            for (var i = 0; i < colorArray.length; i++) {
+                if (getColor === colorArray[i]) {
+                    document.getElementById("easter_egg").style.color = colorArray[(i+1)%colorArray.length];
+                    return;
+                }
+            }
+            console.log(getColor);
+            document.getElementById("easter_egg").style.color = "rgb(255, 0 , 0)";
+        };
         document.getElementById("click_ripples").onclick = function() {
             if (click) {
                 document.getElementById("click_ripples").style.color = "red";
@@ -28,9 +42,30 @@
             }
             click = !click;
         };
+        document.querySelector("input.disappear").onchange = function() {
+            var selected = parseInt(document.querySelector("input.disappear").value);
+            var description = null;
+            if (selected === 1) {
+                disappearTime = 4000;
+                description = "Slowest";
+            } else if (selected === 2) {
+                disappearTime = 2000;
+                description = "Slower";
+            } else if (selected === 3) {
+                disappearTime = 1000;
+                description = "Normal";
+            } else if (selected === 4) {
+                disappearTime = 500;
+                description = "Faster";
+            } else if (selected === 5) {
+                disappearTime = 250;
+                description = "Fastest";
+            }
+            document.querySelector("label.disappear").innerHTML = `Disappearing Time (${description}): `;
+        };
         document.querySelector("input.radius").onchange = function() {
             initRadius = parseInt(document.querySelector("input.radius").value);
-            document.querySelector("label.radius").innerHTML = "Radius (" + initRadius + " px): ";
+            document.querySelector("label.radius").innerHTML = `Radius (${initRadius} px): `;
         };
         document.getElementById("autoripples").onclick = function() {
             if (auto) {
@@ -44,21 +79,49 @@
         };
         document.querySelector("input.create_speed").onchange = function() {
             createSpeed = parseInt(document.querySelector("input.create_speed").value * 1000);
-            document.querySelector("label.create_speed").innerHTML = "Delay (" + createSpeed + " ms): ";
+            document.querySelector("label.create_speed").innerHTML = `Delay (${createSpeed} ms): `;
+        };
+        document.querySelector("input.size_speed").onchange = function() {
+            var selected = parseInt(document.querySelector("input.size_speed").value);
+            var description = null;
+            if (selected === 1) {
+                sizeIncrease = 16;
+                description = "Slowest";
+            } else if (selected === 2) {
+                sizeIncrease = 8;
+                description = "Slower";
+            } else if (selected === 3) {
+                sizeIncrease = 4;
+                description = "Normal";
+            } else if (selected === 4) {
+                sizeIncrease = 2;
+                description = "Faster";
+            } else if (selected === 5) {
+                sizeIncrease = 1;
+                description = "Fastest";
+            }
+            document.querySelector("label.size_speed").innerHTML = `Size Increase (${description}): `;
         };
         document.querySelector("input.color").onchange = function() {
-            colorSpeed = parseInt(document.querySelector("input.color").value);
-            var description = "Slowest";
-            if (colorSpeed <= 10) {
-                description = "Fastest";
-            } else if (colorSpeed <= 20) {
-                description = "Fast";
-            } else if (colorSpeed <= 40) {
-                description = "Normal";
-            } else if (colorSpeed <= 100) {
+            var selected = parseInt(document.querySelector("input.color").value);
+            var description = null;
+            if (selected === 1) {
+                colorSpeed = 100;
+                description = "Slowest";
+            } else if (selected === 2) {
+                colorSpeed = 50;
                 description = "Slower";
+            } else if (selected === 3) {
+                colorSpeed = 25;
+                description = "Normal";
+            } else if (selected === 4) {
+                colorSpeed = 10;
+                description = "Faster";
+            } else if (selected === 5) {
+                colorSpeed = 1;
+                description = "Fastest";
             }
-            document.querySelector("label.color").innerHTML = "Delay (" + description + "): ";
+            document.querySelector("label.color").innerHTML = `Color Change (${description}): `;
         };
         var prevTime = new Date().getTime();
         var lastFrame = new Date().getTime();
@@ -85,7 +148,10 @@
                 y: evt.layerY
             };
             if (!auto) {
-                circles.push(createCircle());
+                if (new Date().getTime() - prevTime > createSpeed) {
+                    circles.push(createCircle());
+                    prevTime = new Date().getTime();
+                }
             }
         }
         function newCircle() {
@@ -125,8 +191,8 @@
                 ctx.lineWidth = 1;
                 ctx.arc(circle.x, circle.y, circle.radius, 0, 2*Math.PI, false);
                 ctx.stroke();
-                circle.transparency -= (new Date().getTime() - lastFrame)/1000;
-                circle.radius += (new Date().getTime() - lastFrame)/5;
+                circle.transparency -= (new Date().getTime() - lastFrame)/disappearTime;
+                circle.radius += (new Date().getTime() - lastFrame)/sizeIncrease;
             }
             lastFrame = new Date().getTime();
             if (!pause) {
