003ca80ef3c7820e843194883b41f6c7a826daf1
Lalith <lalith@uw.edu>
2015-11-21 14:50:49 -0800

    Added site to test out browsers' Geolocation API
    Location.html and location.js created

---
+++ location.js
@@ -0,0 +1,25 @@
+(function() {
+    window.onload = function() {
+        window.setInterval(findLocation, 10000);
+        function findLocation() {
+            navigator.geolocation.getCurrentPosition(addLocation);
+        }
+        function addLocation(pos) {
+            let table = document.getElementById("main_table");
+            let newRow = document.createElement("tr");
+            table.appendChild(newRow);
+            let timeCol = document.createElement("td");
+            timeCol.innerHTML = ("" + new Date()).substring(16,21);
+            newRow.appendChild(timeCol);
+            let latCol = document.createElement("td");
+            latCol.innerHTML = pos.coords.latitude;
+            newRow.appendChild(latCol);
+            let longCol = document.createElement("td");
+            longCol.innerHTML = pos.coords.longitude;
+            newRow.appendChild(longCol);
+            let accCol = document.createElement("td");
+            accCol.innerHTML = pos.coords.accuracy;
+            newRow.appendChild(accCol);
+        }
+    };
+})();
