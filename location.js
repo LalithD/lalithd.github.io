165bf7b4c44a2b3d0ff4199aed3a5df4383490e4
Lalith <lalith@uw.edu>
2015-11-21 17:33:09 -0800

    Fixed Location page and added CSS
    Added CSS and styled page; fixed issue with geolocation API.

--- location.js
+++ location.js
@@ -1,16 +1,23 @@
+/* jshint esnext: true */
 (function() {
     "use strict";
     window.onload = function() {
-        let checkAgain = window.setInterval(findLocation, 1000);
+        let options = {
+            enableHighAccuracy: true, // as accurate as possible
+            timeout: 10000, // must take only 10 sec to respond
+            maximumAge: 0 // must not return cached location
+        };
+        findLocation();
+        //let checkAgain = window.setInterval(findLocation, 10000);
+        let checkAgain = navigator.geolocation.watchPosition(findLocation, error, options);
         function findLocation() {
-            navigator.geolocation.getCurrentPosition(addLocation);
+            navigator.geolocation.getCurrentPosition(addLocation, error, options);
         }
         function addLocation(pos) {
-            let table = document.getElementById("main_table");
             let newRow = document.createElement("tr");
-            table.appendChild(newRow);
+            document.getElementById("table_body").appendChild(newRow);
             let timeCol = document.createElement("td");
-            timeCol.innerHTML = ("" + new Date()).substring(16,21);
+            timeCol.innerHTML = ("" + new Date()).substring(16,24);
             newRow.appendChild(timeCol);
             let latCol = document.createElement("td");
             latCol.innerHTML = pos.coords.latitude;
@@ -19,8 +26,11 @@
             longCol.innerHTML = pos.coords.longitude;
             newRow.appendChild(longCol);
             let accCol = document.createElement("td");
-            accCol.innerHTML = pos.coords.accuracy;
+            accCol.innerHTML = pos.coords.accuracy + " m";
             newRow.appendChild(accCol);
+        }
+        function error() {
+            console.log("An error occurred!");
         }
     };
 })();
